8a8ca7cc8ac2146d901663a84ef84f749ad2ba91
David Riggins <driggins@esri.com>
2022-11-03 17:21:32 -0700

    Lesson 31 End: Custom directives.

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -16,7 +16,7 @@
 
     <div class="edit">
       <h4>Edit counter title:</h4>
-      <input v-model="counterData.title" type="text" />
+      <input v-model="counterData.title" type="text" v-autofocus />
     </div>
   </div>
 </template>
@@ -34,6 +34,7 @@ import {
   onActivated,
   onDeactivated,
 } from "vue";
+import { vAutofocus } from "@/directives/vAutofocus";
 
 // const counter = ref(0);
 // const counterTitle = ref("My Counter:");
@@ -87,6 +88,16 @@ onUnmounted(() => {
 // onActivated(() => {
 //   console.log("onActivated");
 // });
+
+/*
+  Directives
+*/
+// const vAutofocus = {
+//   // v-autofocus
+//   mounted: (el) => {
+//     el.focus();
+//   },
+// };
 </script>
 
 <!-- Options API way for computed and watch -->
