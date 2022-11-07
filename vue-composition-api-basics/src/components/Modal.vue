45a748f1446a34fb0ea4cbc9ddac38215c22e34f
David Riggins <driggins@esri.com>
2022-11-07 13:43:10 -0800

    Lesson 47 End: Child comp, lazy loading, slots, props.

---
+++ vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>{{ title }}</h1>
+      <slot />
+      <!-- <pre>{{ $slots.title() }}</pre> -->
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+// import { useSlots } from "vue";
+
+// const slots = useSlots();
+
+// console.log(slots.title());
+
+/*
+  props
+*/
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "No title specified",
+  },
+});
+
+console.log(props.title);
+</script>
+
+<style>
+.modal {
+  background: beige;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,20 +2,14 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show Modal</button>
-
-    <!-- <teleport to="body"> -->
-    <teleport to=".modals-container">
-      <div v-if="showModal" class="modal">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia
-          nobis tenetur minus architecto necessitatibus saepe consequatur et
-          numquam. Vel asperiores provident omnis veritatis harum corrupti! Aut
-          aspernatur sed eos commodi?
-        </p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal" title="My modal title (via prop)">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia nobis
+        tenetur minus architecto necessitatibus saepe consequatur et numquam.
+        Vel asperiores provident omnis veritatis harum corrupti! Aut aspernatur
+        sed eos commodi?
+      </p>
+    </Modal>
   </div>
 </template>
 
@@ -24,6 +18,7 @@
   imports
 */
 import { ref } from "vue";
+import Modal from "@/components/Modal.vue";
 
 /*
   Modals
@@ -31,16 +26,3 @@ import { ref } from "vue";
 
 const showModal = ref(false);
 </script>
-
-<style>
-.modal {
-  background: beige;
-  padding: 10px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
