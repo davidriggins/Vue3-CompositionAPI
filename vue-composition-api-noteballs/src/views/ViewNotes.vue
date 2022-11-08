3112a9d0961b570478ae069a4ba32124a1049210
David Riggins <driggins@esri.com>
2022-11-08 11:42:20 -0800

    Lesson 86 End: Add Notes array and Add Note method.

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -3,26 +3,32 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add New Note
           </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" class="card mb-4">
+    <div v-for="note in notes" :key="note.id" class="card mb-4">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quidem
-          distinctio repellendus blanditiis neque optio, ipsum minima eum magnam
-          eaque, voluptates accusamus nobis quisquam deserunt. Numquam quod ipsa
-          voluptas quasi!
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -32,3 +38,44 @@
     </div>
   </div>
 </template>
+
+<script setup>
+/*
+  imports
+*/
+import { ref } from "vue";
+/*
+  notes
+*/
+const newNote = ref("");
+const newNoteRef = ref(null);
+
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quide distinctio repellendus blanditiis neque optio, ipsum minima eum magnam eaque, voluptates accusamus nobis quisquam deserunt. Numquam quod ipsa voluptas quasi!",
+  },
+  {
+    id: "id2",
+    content: "This is a shorter note!",
+  },
+]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime();
+  let id = currentDate.toString();
+
+  let note = {
+    id: id,
+    content: newNote.value,
+  };
+
+  // unshift puts it to the bottom of the array
+  notes.value.unshift(note);
+
+  newNote.value = "";
+
+  newNoteRef.value.focus();
+};
+</script>
