6cec6b85524eff624bdc08576a9d409056ff1da0
David Riggins <driggins@esri.com>
2022-11-08 17:13:59 -0800

    Lesson 100 End: Work on Note Editing.

--- vue-composition-api-noteballs/src/router/index.js
+++ vue-composition-api-noteballs/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ViewNotes from "@/views/ViewNotes.vue";
+import ViewEditNote from "@/views/ViewEditNote.vue";
 import ViewStats from "@/views/ViewStats.vue";
 
 import "@/assets/main.css";
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "notes",
     component: ViewNotes,
+  },
+  {
+    path: "/editNote/:id",
+    name: "edit-note",
+    component: ViewEditNote,
   },
   {
     path: "/stats",
