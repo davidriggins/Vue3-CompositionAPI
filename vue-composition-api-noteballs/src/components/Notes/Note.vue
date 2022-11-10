b1ae84bf011a4ae852acbe3c44a921ae4f4c1213
David Riggins <driggins@esri.com>
2022-11-10 14:30:48 -0800

    Lesson 119 End: Delete note.

--- vue-composition-api-noteballs/src/components/Notes/Note.vue
+++ vue-composition-api-noteballs/src/components/Notes/Note.vue
@@ -20,7 +20,11 @@
       >
     </footer>
 
-    <ModalDeleteNote v-if="modals.deleteNote" v-model="modals.deleteNote" />
+    <ModalDeleteNote
+      v-if="modals.deleteNote"
+      v-model="modals.deleteNote"
+      :noteId="note.id"
+    />
   </div>
 </template>
 
