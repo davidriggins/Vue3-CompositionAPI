e5b916ce3bc7361558e73d4b2a955075e44c6b3d
David Riggins <driggins@esri.com>
2023-03-30 14:24:15 -0700

    Finalized version.

--- vue-composition-api-noteballs.mine/src/components/Notes/ModalDeleteNote.vue
+++ vue-composition-api-noteballs.mine/src/components/Notes/ModalDeleteNote.vue
@@ -6,14 +6,10 @@
         <p class="modal-card-title">Delete Note?</p>
         <button @click="closeModal" class="delete" aria-label="close"></button>
       </header>
-      <section class="modal-card-body">
-        Are you sure you want to delete this note?
-      </section>
+      <section class="modal-card-body">Are you sure you want to delete this note?</section>
       <footer class="modal-card-foot is-justify-content-flex-end">
         <button @click="closeModal" class="button">Cancel</button>
-        <button @click="storeNotes.deleteNote(noteId)" class="button is-danger">
-          Delete
-        </button>
+        <button @click="storeNotes.deleteNote(noteId)" class="button is-danger">Delete</button>
       </footer>
     </div>
   </div>
